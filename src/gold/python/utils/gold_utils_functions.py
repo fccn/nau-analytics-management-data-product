535305ccf4bd149c215fd06a4fc9f0c30f95d421
paulormonteiro <paulo.r.monteiro@glinttglobal.com>
2026-04-08 14:58:25 +0100

    Fixed bug

--- src/gold/python/utils/gold_utils_functions.py
+++ src/gold/python/utils/gold_utils_functions.py
@@ -16,7 +16,7 @@ def get_max_timestamp_for_table(spark_session: SparkSession, table_name: str, en
     try:
         START_DATE = spark_session.sql(
             f"SELECT NVL(max(last_execution_ts),'1900-01-01 00:00:00') as ts "
-            f"FROM bronze{env}.audit.pipeline_run_ctrl "
+            f"FROM gold{env}.audit.pipeline_run_ctrl "
             f"WHERE table_name = '{table_name}' and pipeline = 'gestao'"
         ).first()["ts"]
     except Exception:
@@ -27,7 +27,7 @@ def get_max_timestamp_for_table(spark_session: SparkSession, table_name: str, en
 def update_ctrl_table(spark_session: SparkSession, table_name: str, current_timestamp: str, number_of_records: int, env: str) -> bool:
     try:
         spark_session.sql(f"""
-            INSERT INTO bronze{env}.audit.pipeline_run_ctrl
+            INSERT INTO gold{env}.audit.pipeline_run_ctrl
             VALUES('gestao', '{table_name}', '{current_timestamp}', {number_of_records})
             """)
         return True
