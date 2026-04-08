38f406c4c0be8312912211b0d8e02a348c6ec9a5
paulormonteiro <paulo.r.monteiro@glinttglobal.com>
2026-04-08 12:55:20 +0100

    Fixed bug

--- src/bronze/python/bronze_downtimes_ingestion.py
+++ src/bronze/python/bronze_downtimes_ingestion.py
@@ -18,7 +18,6 @@ logging.basicConfig(
     ]
 )
 
-
 def get_google_service_sheet(service_account_json:str) -> Any:
     SCOPES = [
         "https://www.googleapis.com/auth/drive.readonly",
@@ -35,14 +34,14 @@ def get_google_service_sheet(service_account_json:str) -> Any:
 def get_google_sheet_as_pd_df(google_sheet_id: str,sheets_service: Any, renamed_columns:Dict[str,str]) -> pd.DataFrame:
     RANGE = "A:Z"
     result = sheets_service.spreadsheets().values().get(
-    spreadsheetId=google_sheet_id,
-    range=RANGE
+        spreadsheetId=google_sheet_id,
+        range=RANGE
     ).execute()
     values = result.get("values", [])
     df = pd.DataFrame(values[1:], columns=values[0])
     df = df[df['From (Lisbon time)'] != ""]
     df = df.rename(columns=renamed_columns) #type: ignore
-    return df  
+    return df 
 
 def create_spark_df_from_pandas_df(spark: SparkSession , pd_df: pd.DataFrame) -> DataFrame:
     spark_df = spark.createDataFrame(pd_df)
@@ -75,9 +74,9 @@ def get_max_timestamp_for_table(spark_session: SparkSession, table_name:str,env:
 def update_ctrl_table(spark_session: SparkSession, table_name:str,current_timestamp: str ,number_of_records:int,env:str) -> bool:
     try:
         spark_session.sql(f"""
-            INSERT INTO  bronze{env}.audit.pipeline_run_ctrl
+            INSERT INTO bronze{env}.audit.pipeline_run_ctrl
             VALUES('gestao', '{table_name}', '{current_timestamp}', {number_of_records})
-        """)
+            """)
         return True 
     except Exception:
         return False
@@ -86,15 +85,14 @@ def add_ingestion_metadata_column(df: DataFrame,table: str,current_timestamp:str
     tmp_df = df.withColumn("ingestion_date", F.lit(current_timestamp)).withColumn("source_name", F.lit(table))
     return tmp_df
 
-
 def validate_ingestion_values(spark_session:SparkSession,src_table_df: DataFrame,table_name:str,env:str) -> int:
-        src_count = src_table_df.count()
-        tgt_count = spark_session.sql(F"SELECT count(*)  as count FROM bronze{env}.gestao.{table_name}").first()["count"]
-        if src_count != tgt_count:
-            raise Exception(
-                f"Count mismatch! Source = {src_count}, Target = {tgt_count}. Aborting pipeline."
-            )
-        return tgt_count
+    src_count = src_table_df.count()
+    tgt_count = spark_session.sql(f"SELECT count(*) as count FROM bronze{env}.gestao.{table_name}").first()["count"]
+    if src_count != tgt_count:
+        raise Exception(
+            f"Count mismatch! Source = {src_count}, Target = {tgt_count}. Aborting pipeline."
+        )
+    return tgt_count
 
 def main():
     #Define variables
@@ -105,44 +103,65 @@ def main():
     GOOGLE_ACCOUNT_JSON = base64.b64decode(GOOGLE_ACCOUNT_JSON).decode()
     GOOGLE_SHEET_ID = base64.b64decode(GOOGLE_SHEET_ID).decode()
     service_sheet = get_google_service_sheet(GOOGLE_ACCOUNT_JSON)
-    
-    
+
     renamed_columns = {'From (Lisbon time)':"from_lisbon_time", 
-    'To (Lisbon time)':"to_lisbon_time", 
-    'Impact':"impact", 
-    'Duration':"duration",
-    'Duration in minutes':"duration_minutes", 
-    'Expected':"expected", 
-    'Detected by Nagios':"detected_by_nagios",
-    'Detected by Icinga':"detected_by_icinga", 
-    'Detected by UptimeRobot':"detected_by_uptimerobot", 
-    'Description':"description",
-    'Affected Applications':"affected_applications", 
-    'lms.nau.edu.pt & studio.nau.edu.pt':"lms_nau_edu_pt_and_studio_nau_edu_pt",
-    'www.nau.edu.pt':"www_nau_edu_pt", 
-    'Only some sub-service(s) affected':"only_some_sub_service_affected"}
-    
+                       'To (Lisbon time)':"to_lisbon_time", 
+                       'Impact':"impact", 
+                       'Duration':"duration",
+                       'Duration in minutes':"duration_minutes", 
+                       'Expected':"expected", 
+                       'Detected by Nagios':"detected_by_nagios",
+                       'Detected by Icinga':"detected_by_icinga", 
+                       'Detected by UptimeRobot':"detected_by_uptimerobot", 
+                       'Description':"description",
+                       'Affected Applications':"affected_applications", 
+                       'lms.nau.edu.pt & studio.nau.edu.pt':"lms_nau_edu_pt_and_studio_nau_edu_pt",
+                       'www.nau.edu.pt':"www_nau_edu_pt", 
+                       'Only some sub-service(s) affected':"only_some_sub_service_affected"}
+
     tgt_layer = f"bronze{ENVIRONMENT}"
     tgt_pipeline = "gestao"
     tgt_table_name = "downtimes"
-    
+
     #start processing
     spark = start_iceberg_session("Downtimes_ingestion")
 
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS bronze{ENVIRONMENT}.gestao.downtimes (
+            from_lisbon_time TIMESTAMP,
+            to_lisbon_time TIMESTAMP,
+            impact STRING,
+            duration STRING,
+            duration_minutes INT,
+            expected BOOLEAN,
+            detected_by_nagios BOOLEAN,
+            detected_by_icinga BOOLEAN,
+            detected_by_uptimerobot BOOLEAN,
+            description STRING,
+            affected_applications STRING,
+            lms_nau_edu_pt_and_studio_nau_edu_pt BOOLEAN,
+            www_nau_edu_pt BOOLEAN,
+            only_some_sub_service_affected BOOLEAN,
+            ingestion_date TIMESTAMP NOT NULL,
+            source_name STRING NOT NULL
+        ) USING ICEBERG;
+    """)
+
     pd_df = get_google_sheet_as_pd_df(google_sheet_id=GOOGLE_SHEET_ID,sheets_service=service_sheet,renamed_columns=renamed_columns)
     df = create_spark_df_from_pandas_df(spark=spark,pd_df=pd_df)
-    
-
 
     current_timestamp = spark.sql("SELECT current_timestamp() as c").first()["c"]
-    start_date = get_max_timestamp_for_table(spark,tgt_table_name, ENVIRONMENT)
+    start_date = get_max_timestamp_for_table(spark, tgt_table_name, ENVIRONMENT)
     unfiltered_df = df
-    df = df.filter(f"from_lisbon_time >= {start_date}")
+    df = df.filter(f"from_lisbon_time >= '{start_date}'")
     df = add_ingestion_metadata_column(df, tgt_table_name, current_timestamp)
     df.write.format("iceberg").mode("append").saveAsTable(f"{tgt_layer}.{tgt_pipeline}.{tgt_table_name}")
-    
+
     nr = validate_ingestion_values(spark_session=spark,src_table_df=unfiltered_df,table_name=tgt_table_name,env=ENVIRONMENT)
-    
-    result = update_ctrl_table(spark_session=spark,table_name=tgt_table_name,current_timestamp = current_timestamp,env=ENVIRONMENT,number_of_records=nr)
+
+    result = update_ctrl_table(spark_session=spark,table_name=tgt_table_name,current_timestamp=current_timestamp,env=ENVIRONMENT,number_of_records=nr)
     if not result:
         raise Exception("ERROR UPDATING METADATA TABLE")
+
+if __name__ == "__main__":
+    main()
